6df5e07e6e6383fcee5ecf20fbafd10ec12c19f1
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-06-01 11:55:47 -0600

    adding build

---
+++ build/precache-manifest.a5d101c1d5f5a743b6bc00927562fd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ded5192121d92c61c7e2408722dbfd7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "66f9d44b68f771583257",
+    "url": "/portfolio/static/css/main.71b63af0.chunk.css"
+  },
+  {
+    "revision": "77d856c4b7fcf7e36969",
+    "url": "/portfolio/static/js/2.fddd8d12.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.fddd8d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66f9d44b68f771583257",
+    "url": "/portfolio/static/js/main.fde7a86e.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "72ba7d2fbb8b789ad0920e1d7cb6aa95",
+    "url": "/portfolio/static/media/sam.72ba7d2f.png"
+  }
+]);
