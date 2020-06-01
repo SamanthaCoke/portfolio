8ec7d5c3ecc31687fe0c80d241029dd49c536294
SamanthaCoke <57117836+SamanthaCoke@users.noreply.github.com>
2020-06-01 11:42:57 -0600

    Updates

---
+++ precache-manifest.e565268a8396f47d8bf088411d663f46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab01489a0cffa6160d047e987e59a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4637d4b75b309936185",
+    "url": "/static/css/main.71b63af0.chunk.css"
+  },
+  {
+    "revision": "77d856c4b7fcf7e36969",
+    "url": "/static/js/2.fddd8d12.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.fddd8d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4637d4b75b309936185",
+    "url": "/static/js/main.c2f030ba.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "72ba7d2fbb8b789ad0920e1d7cb6aa95",
+    "url": "/static/media/sam.72ba7d2f.png"
+  }
+]);
